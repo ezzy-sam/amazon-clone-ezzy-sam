1107076a3ecc447bd4cd7c7a2ef98e94007a352f
ezzy-sam <gebremichaelsamson78@gmail.com>
2025-03-06 20:56:54 +0000

    last commit

--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -1,28 +1,4 @@
-// import firebase from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// //authentication 
-// import {getAuth} from "firebase/auth"
-// import "firebase/compact/firestore"
-// import "firebase/compact/auth"
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDxE46nbPShld2FHyFVZD18KT2jLL5Jr-U",
-//   authDomain: "clone-2e9c9.firebaseapp.com",
-//   projectId: "clone-2e9c9",
-//   storageBucket: "clone-2e9c9.firebasestorage.app",
-//   messagingSenderId: "187016754380",
-//   appId: "1:187016754380:web:610b6bf4553e001234fe69",
-// };
 
-// // Initialize Firebase
-// const app = firebase.initializeApp(firebaseConfig);
-// export const auth = getAuth(app)
-// export const db = app.firestore()
-
-
-
-
-// Import the necessary Firebase modules from Firebase v9+ SDK
 
 import firebase from "firebase/compat/app";
 import { initializeApp } from "firebase/app"; 
